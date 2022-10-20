6ae13909c63bc0bd04744146f2b84f46b92afcfd
BRIDALNET\hagi-ya <hagi-ya@ibjapan.jp>
2022-10-20 14:03:25 +0900

    一旦

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
@@ -10,20 +10,47 @@ const Main = (): JSX.Element => {
   const [count, setCount] = useState(0);
   // 関数定義
 
+  useEffect(() => {
+    const pushButtonDakoku = function (): void {
+      (document.querySelector(
+        "#stampPop > button:nth-child(2)"
+      ) as HTMLInputElement).disabled = true;
+    };
+    const dakoku = document.querySelector(
+      "#header > dl:nth-child(2) > dd > button"
+    );
+    dakoku?.setAttribute("onclick", "stampFn();pushButtonDakoku()");
+  }, []);
+
   const everyTimeDo = function (): void {
-    if (document.getElementsByName("login")[0] !== undefined) {
+    //autocomplete input value 取れない
+    console.log(location.pathname === "/ibj/login");
+    console.log(document.querySelector("#username") !== undefined);
+    console.log(
+      (document.querySelector("#username") as HTMLInputElement).value
+    );
+    if (
+      location.pathname === "/ibj/login" &&
+      document.querySelector("#username") !== undefined &&
+      (document.querySelector("#username") as HTMLInputElement).value !== ""
+    ) {
       document.getElementsByName("login")[0].click();
     }
   };
 
-  const inputKyujithu = function (): void {
-    if (typeof jQuery !== "undefined") {
-      // jQueryの読み込み確認
-      $(function () {
-        alert("jQuery is ready.");
-      });
-    }
+  const pushButtonDakoku = function (): void {
+    (document.querySelector(
+      "#stampPop > button:nth-child(2)"
+    ) as HTMLInputElement).disabled = true;
+  };
+  const stop = function (): void {
+    const dakoku = document.querySelector(
+      "#header > dl:nth-child(2) > dd > button"
+    );
+    dakoku?.setAttribute("onclick", "pushButton()");
+  };
 
+  const inputKyujithu = function (): void {
     const trTags = document
       .querySelector("#list_att_add > tbody")
       ?.getElementsByTagName("tr");
@@ -47,43 +74,6 @@ const Main = (): JSX.Element => {
     }
   };
 
-  const start = useCallback(() => {
-    if (intervalRef.current !== null) {
-      return;
-    }
-    intervalRef.current = setInterval(() => {
-      setCount((c) => c + 1);
-      swipe();
-    }, Math.random() * 4000 + 1000);
-  }, []);
-
-  const stop = useCallback(() => {
-    if (intervalRef.current === null) {
-      return;
-    }
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-  }, []);
-
-  const swipe = function (): void {
-    ran = Math.random();
-    const result = Math.floor(ran * 100);
-    if (result > 84) {
-      const badElem = document.querySelector(
-        "#s1362659109 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.Mt\\(a\\).Px\\(4px\\)--s.Pos\\(r\\).Expand.H\\(--recs-card-height\\)--ml.Maw\\(--recs-card-width\\)--ml > div > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-nope-default\\) > button"
-      );
-      if (badElem === null) return;
-      (badElem as HTMLElement).click();
-    } else {
-      const goodElem = document.querySelector(
-        "#s1362659109 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.Mt\\(a\\).Px\\(4px\\)--s.Pos\\(r\\).Expand.H\\(--recs-card-height\\)--ml.Maw\\(--recs-card-width\\)--ml > div > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-like-default\\) > button > span > span"
-      );
-      if (goodElem === null) return;
-      (goodElem as HTMLElement).click();
-    }
-  };
-
-  everyTimeDo();
   return (
     <Stack direction="row" spacing={2}>
       <Button color="secondary">{count}</Button>
